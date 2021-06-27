8ba28a11785fdd4f3f7f04e760730cff766dae98
Connor Wang <xw345@cam.ac.uk>
2021-06-27 02:29:00 +0100

    logout update

--- src/components/SignOut/index.js
+++ src/components/SignOut/index.js
@@ -12,8 +12,6 @@ class SignOutButton extends React.Component {
 
   SignOut(){
     this.props.firebase.doSignOut();
-    fetch("/raven/logout");
-    alert("You have successfully logged out from Raven!")
     this.props.history.push("/");
   }
 
