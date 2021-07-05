b1bba9910ca68780e8e18535ed7b850cb0a4b17d
Connor Wang <xw345@cam.ac.uk>
2021-07-05 18:00:51 +0100

    remove the footer

--- src/components/Desktop/Reviews/index.js
+++ src/components/Desktop/Reviews/index.js
@@ -3,13 +3,17 @@ import './index.css';
 import Navigation from "../Navigation";
 import Footer from "../Footer";
 import React from "react";
+import * as ROUTES from "../../../constants/routes";
 
 const Reviews = () => {
   return (
     <div className="Reviews">
       <Navigation />
       <Posts />
-      <Footer />
+      <div className="report-issues">
+        <p>Copyright &copy; 2021 ｜ <a href={ROUTES.REPORT}> Report an issue here</a> or contact us at <a
+          href={"mailto:admin@camments.com"}>admin@camments.com</a></p>
+      </div>
     </div>
   );
 }
